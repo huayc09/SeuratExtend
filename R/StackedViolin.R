8ef86daa6ac9844d0dae3c9d5db9f30a0f8b2c38
Yichao Hua <61422457+huayc09@users.noreply.github.com>
2020-04-09 13:29:23 +0200

    Update stacked voilin

--- R/StackedViolin.R
+++ R/StackedViolin.R
@@ -2,6 +2,7 @@
 #' @description FUNCTION_DESCRIPTION
 #' @param matr PARAM_DESCRIPTION
 #' @param f PARAM_DESCRIPTION
+#' @param f2 PARAM_DESCRIPTION, Default: NULL
 #' @param features PARAM_DESCRIPTION, Default: NULL
 #' @param ncol PARAM_DESCRIPTION, Default: 1
 #' @param lab_fill PARAM_DESCRIPTION, Default: 'group'
@@ -19,7 +20,7 @@
 #' @export
 #' @importFrom reshape2 melt
 
-StackedViolin <- function(matr, f, features = NULL, ncol = 1, lab_fill = "group"){
+StackedViolin <- function(matr, f, f2 = NULL, features = NULL, ncol = 1, lab_fill = "group"){
   require(ggplot2)
   require(rlang)
   require(dplyr)
@@ -28,20 +29,35 @@ StackedViolin <- function(matr, f, features = NULL, ncol = 1, lab_fill = "group"
     message(paste0(setdiff(features, rownames(matr)), collapse = ", "), " not found")
     features <- intersect(features, rownames(matr))
   }
+  f2 <- f2 %||% data.frame(row.names = colnames(matr))
   ToPlot <-
-    cbind(f, as.data.frame(t(matr[features,]))) %>%
-    reshape2::melt()
-  p<-ggplot(ToPlot, aes(x=f,y=value, fill=f))+
-    geom_violin(scale = "width")+
-    facet_wrap( ~variable, ncol = ncol, strip.position="left")+
-    ylab(NULL) +
-    xlab(NULL) +
-    theme(strip.background = element_blank(),
-          strip.placement = "outside",
-          legend.position = "none",
-          axis.text.x=element_text(angle = 45,hjust = 1)) +
-    theme_classic() +
-    labs(fill = lab_fill)
+    cbind(f, f2, as.data.frame(t(matr[features,]))) %>%
+    reshape2::melt(measure.vars = features)
+  if(is_empty(f2)){
+    p<-ggplot(ToPlot, aes(x=f,y=value, fill=f))+
+      geom_violin(scale = "width")+
+      facet_wrap( ~variable, ncol = ncol, strip.position="left")+
+      ylab(NULL) +
+      xlab(NULL) +
+      theme(strip.background = element_blank(),
+            strip.placement = "outside",
+            legend.position = "none",
+            axis.text.x=element_text(angle = 45,hjust = 1)) +
+      theme_classic() +
+      labs(fill = lab_fill)
+  }else{
+    p<-ggplot(ToPlot, aes(x=f,y=value, fill=f))+
+      geom_violin(scale = "width")+
+      facet_grid(vars(variable), vars(f2), switch = c("both"))+
+      ylab(NULL) +
+      xlab(NULL) +
+      theme(strip.background = element_blank(),
+            strip.placement = "outside",
+            legend.position = "none",
+            axis.text.x=element_text(angle = 45,hjust = 1)) +
+      theme_classic()
+  }
+
   return(p)
 }
 
@@ -50,6 +66,7 @@ StackedViolin <- function(matr, f, features = NULL, ncol = 1, lab_fill = "group"
 #' @param seu PARAM_DESCRIPTION
 #' @param features PARAM_DESCRIPTION
 #' @param group.by PARAM_DESCRIPTION, Default: 'seurat_clusters'
+#' @param split.by PARAM_DESCRIPTION, Default: NULL
 #' @param cell PARAM_DESCRIPTION, Default: NULL
 #' @param ncol PARAM_DESCRIPTION, Default: 1
 #' @return OUTPUT_DESCRIPTION
@@ -63,17 +80,25 @@ StackedViolin <- function(matr, f, features = NULL, ncol = 1, lab_fill = "group"
 #' @rdname StackedViolin_v3
 #' @export
 
-StackedViolin_v3 <- function(seu, features, group.by = "seurat_clusters", cell = NULL, ncol = 1){
+StackedViolin_v3 <- function(seu, features, group.by = "seurat_clusters", split.by = NULL, cell = NULL, ncol = 1){
   require(rlang)
   cell <- cell %||% colnames(seu)
-  matr <- GetAssayData(seu)[features, cell]
+  matr <- as.matrix(GetAssayData(seu))[features, cell]
   f <- factor(seu[[group.by]][cell,])
-  p <- StackedViolin(matr, f, features, ncol, lab_fill = group.by)
+  f2 <- seu[[split.by]][cell,]
+  p <- StackedViolin(matr, f, f2, features, ncol, lab_fill = group.by)
   return(p)
 }
 
+# seu <- readRDS("~/R documents/2020-2-10 EC PyMT and E0771/rds/PyMTEC_old.rds")
 # matr <- as.matrix(GetAssayData(seu))[1:20,]
 # f <- seu@meta.data$cluster
-# features = NULL
-# StackedViolin(matr, f, ncol = 2)
-# StackedViolin_v3(seu = seu, features = features)
+# f2 <- NULL
+# features = c("Selp","Sele","Vwf","Glycam1")
+# cell = NULL
+# split.by = "orig.ident"
+# group.by = "seurat_clusters"
+# ncol = 1
+# lab_fill = "group"
+# StackedViolin(matr, f, f2, ncol = 2)
+# StackedViolin_v3(seu = seu, features = features, split.by = split.by)
--- data-raw/Test.R
+++ data-raw/Test.R
@@ -35,7 +35,7 @@ only.end.terms = T
 slot = "counts"
 assay = "RNA"
 nCores = 4
-makeOxygen(GeneSetAnalysis)
+makeOxygen(StackedViolin_v3)
 roxygenize()
 CalcScoreGeneral_v3(Seu, features, group.by, "zscore")
 
