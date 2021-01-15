ed046674ea0a8efe57343d6d21978bc0959714da
Yichao Hua <61422457+huayc09@users.noreply.github.com>
2021-01-15 17:15:42 +0100

    2021-1-15 update GenesymbolConversion
    Update GenesymbolConversion:
    The function support both vector and gene matrix conversion

--- R/GenesymbolConversion.R
+++ R/GenesymbolConversion.R
@@ -3,6 +3,7 @@
 #' @param human_genes PARAM_DESCRIPTION
 #' @param mirror PARAM_DESCRIPTION, Default: NULL
 #' @param local.mode PARAM_DESCRIPTION, Default: T
+#' @param keep.seq PARAM_DESCRIPTION, Default: F
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @examples
@@ -14,20 +15,37 @@
 #' @rdname HumanToMouseGenesymbol
 #' @export
 
-HumanToMouseGenesymbol <- function(human_genes, mirror = NULL, local.mode = T){
-  if(local.mode){
-    genelist <- mouse_human_genesymbols[mouse_human_genesymbols$HGNC.symbol %in% human_genes, 2:1]
-  } else {
-    human_genes <- unique(human_genes)
-    import("biomaRt")
-    message("Posible mirrors: 'www', 'uswest', 'useast', 'asia'.")
-    human <- useDataset("hsapiens_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
-    mouse <- useDataset("mmusculus_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
-    genelist = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = human_genes, mart = human,
-                      attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+HumanToMouseGenesymbol <- function(human_genes, mirror = NULL, local.mode = T, keep.seq = F){
+  if(is.vector(human_genes)){
+    if(local.mode){
+      genelist <- mouse_human_genesymbols[mouse_human_genesymbols$HGNC.symbol %in% human_genes, 2:1]
+    } else {
+      human_genes <- unique(human_genes)
+      import("biomaRt")
+      message("Posible mirrors: 'www', 'uswest', 'useast', 'asia'.")
+      human <- useDataset("hsapiens_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
+      mouse <- useDataset("mmusculus_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
+      genelist = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = human_genes, mart = human,
+                        attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+    }
+    if(nrow(genelist) == 0) message("No homologous genes found")
+    if(keep.seq) {
+      genelist2 <- vector()
+      for (i in human_genes) {
+        genelist2[i] <- genelist$MGI.symbol[genelist$HGNC.symbol %in% i][1]
+        genelist <- genelist[!genelist$HGNC.symbol %in% i & !genelist$MGI.symbol %in% genelist2[i],]
+      }
+      return(genelist2)
+    }
+    return(genelist)
+  }else{
+    library(magrittr)
+    library(dplyr)
+    human_genes <- human_genes %>%
+      set_rownames(HumanToMouseGenesymbol(rownames(.), local.mode = T, keep.seq = T)) %>%
+      .[!is.na(rownames(.)),]
+    return(human_genes)
   }
-  if(nrow(genelist) == 0) message("No homologous genes found")
-  return(genelist)
 }
 
 #' @title FUNCTION_TITLE
@@ -35,6 +53,7 @@ HumanToMouseGenesymbol <- function(human_genes, mirror = NULL, local.mode = T){
 #' @param mouse_genes PARAM_DESCRIPTION
 #' @param mirror PARAM_DESCRIPTION, Default: NULL
 #' @param local.mode PARAM_DESCRIPTION, Default: T
+#' @param keep.seq PARAM_DESCRIPTION, Default: F
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @examples
@@ -46,19 +65,36 @@ HumanToMouseGenesymbol <- function(human_genes, mirror = NULL, local.mode = T){
 #' @rdname MouseToHumanGenesymbol
 #' @export
 
-MouseToHumanGenesymbol <- function(mouse_genes, mirror = NULL, local.mode = T){
-  if(local.mode){
-    genelist <- mouse_human_genesymbols[mouse_human_genesymbols$MGI.symbol %in% mouse_genes, ]
-  } else {
-    mouse_genes <- unique(mouse_genes)
-    import("biomaRt")
-    message("Posible mirrors: 'www', 'uswest', 'useast', 'asia'.")
-    human <- useDataset("hsapiens_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
-    mouse <- useDataset("mmusculus_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
-    genelist = getLDS(attributesL = c("hgnc_symbol"), filters = "mgi_symbol", values = mouse_genes, martL = human,
-                      attributes = c("mgi_symbol"), mart = mouse, uniqueRows=T)
+MouseToHumanGenesymbol <- function(mouse_genes, mirror = NULL, local.mode = T, keep.seq = F){
+  if(is.vector(mouse_genes)){
+    if(local.mode){
+      genelist <- mouse_human_genesymbols[mouse_human_genesymbols$MGI.symbol %in% mouse_genes, ]
+    } else {
+      mouse_genes <- unique(mouse_genes)
+      import("biomaRt")
+      message("Posible mirrors: 'www', 'uswest', 'useast', 'asia'.")
+      human <- useDataset("hsapiens_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
+      mouse <- useDataset("mmusculus_gene_ensembl", useEnsembl(biomart = "ensembl", mirror = mirror))
+      genelist = getLDS(attributesL = c("hgnc_symbol"), filters = "mgi_symbol", values = mouse_genes, martL = human,
+                        attributes = c("mgi_symbol"), mart = mouse, uniqueRows=T)
+    }
+    if(nrow(genelist) == 0) message("No homologous genes found")
+    if(keep.seq) {
+      genelist2 <- vector()
+      for (i in mouse_genes) {
+        genelist2[i] <- genelist$HGNC.symbol[genelist$MGI.symbol %in% i][1]
+        genelist <- genelist[!genelist$MGI.symbol %in% i & !genelist$HGNC.symbol %in% genelist2[i],]
+      }
+      return(genelist2)
+    }
+    return(genelist)
+  }else{
+    library(magrittr)
+    library(dplyr)
+    mouse_genes <- mouse_genes %>%
+      set_rownames(MouseToHumanGenesymbol(rownames(.), local.mode = T, keep.seq = T)) %>%
+      .[!is.na(rownames(.)),]
+    return(mouse_genes)
   }
-  if(nrow(genelist) == 0) message("No homologous genes found")
-  return(genelist)
 }
 
